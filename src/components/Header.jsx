d38427b7f447e273f9e63405237930c99ae251f8
matt6gsr <matt6gsr@gmail.com>
2021-02-03 21:47:04 +0000

    Header component created

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HeaderWrapper, HeaderText } from '../styles/Header';
+
+export const Header = () => {
+  return (
+    <HeaderWrapper>
+      <HeaderText>Create Your List Of People</HeaderText>
+    </HeaderWrapper>
+  );
+};
---
+++ src/styles/Header.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HeaderText = styled.h3`
+  color: #3e3e45;
+`;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
