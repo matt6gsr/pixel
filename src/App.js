a120b1ae66b2d64f218a210f39aeedf12b27ffb3
matt6gsr <matt6gsr@gmail.com>
2021-02-04 00:58:15 +0000

    imported list components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { AppWrapper, ContentWrapper } from './styles/App';
 import { Header } from './components/Header';
+import DataList from './components/DataList';
+import UserCreatedList from './components/UserCreatedList';
 
 function App() {
   return (
     <AppWrapper>
-      <Header />
-      <ContentWrapper></ContentWrapper>
+      <Header text="Create Your List Of People" />
+      <ContentWrapper>
+        <DataList />
+        <UserCreatedList />
+      </ContentWrapper>
     </AppWrapper>
   );
 }
