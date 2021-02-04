35564f3e0cbfe72da73a7d802f8423e9579ada1c
matt6gsr <matt6gsr@gmail.com>
2021-02-04 00:31:37 +0000

    created common styles file

---
+++ src/styles/commom.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ListWrapper = styled.div`
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid lightgrey;
+  border-radius: 5px;
+  width: 50%;
+  background-color: white;
+`;
