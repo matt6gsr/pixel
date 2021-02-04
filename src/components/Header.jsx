b6b12026ed42960b7a18327181c4e04d36877bd7
matt6gsr <matt6gsr@gmail.com>
2021-02-04 21:49:19 +0000

    added SortButton and sort logic (incomplete)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { HeaderWrapper, HeaderText } from '../styles/Header';
+import { SortButton } from '../styles/commom';
 
-export const Header = ({ text, list }) => {
+export const Header = ({ text, list, userList, setUserList }) => {
+  console.log('userList in Header', userList);
+
   return (
     <HeaderWrapper>
       <HeaderText list={list}>{text}</HeaderText>
+      {userList && userList.length > 1 && (
+        <SortButton
+          onClick={() => {
+            console.log('fired');
+            setUserList((userList) =>
+              userList.sort((a, b) => a.name.localeCompare(b.name))
+            );
+            console.log('userList in func', userList);
+          }}
+        >
+          A {`>`} Z
+        </SortButton>
+      )}
     </HeaderWrapper>
   );
 };
