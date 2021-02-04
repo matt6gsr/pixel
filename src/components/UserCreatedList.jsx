b4a206bcabad33d30a0772d5f98a6b35d1ac4428
matt6gsr <matt6gsr@gmail.com>
2021-02-04 00:57:45 +0000

    edited export syntax

--- src/components/UserCreatedList.jsx
+++ src/components/UserCreatedList.jsx
@@ -1,10 +1,12 @@
 import { ListWrapper } from '../styles/commom';
 import { Header } from './Header';
 
-export const UserCreatedList = () => {
+const UserCreatedList = () => {
   return (
     <ListWrapper>
       <Header text="Please click a person to remove them from your list" />
     </ListWrapper>
   );
 };
+
+export default UserCreatedList;
