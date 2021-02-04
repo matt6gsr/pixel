f911fd3ae8bc43d0621d2fa0e9ad0f55f936690f
matt6gsr <matt6gsr@gmail.com>
2021-02-04 00:53:02 +0000

    basic user-created-list component created

---
+++ src/components/UserCreatedList.jsx
@@ -0,0 +1,10 @@
+import { ListWrapper } from '../styles/commom';
+import { Header } from './Header';
+
+export const UserCreatedList = () => {
+  return (
+    <ListWrapper>
+      <Header text="Please click a person to remove them from your list" />
+    </ListWrapper>
+  );
+};
