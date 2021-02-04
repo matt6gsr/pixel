32b661ac5a5d6d1ff5efd0c7ee7e75c6e94a79f6
matt6gsr <matt6gsr@gmail.com>
2021-02-04 13:52:36 +0000

    styled PersonCard

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { CardWrapper } from '../styles/PersonCard';
+import {
+  CardWrapper,
+  Content,
+  Avatar,
+  Name,
+  Email,
+  Button,
+} from '../styles/PersonCard';
 
 const PersonCard = ({ person }) => {
   return (
     <CardWrapper>
-      <h4>{person.name}</h4>
+      <Avatar
+        src={'https://i.pravatar.cc/150?u=' + person.email}
+        alt={person.name}
+      />
+      <Content>
+        <Name>{person.name}</Name>
+        <Email>{person.email}</Email>
+      </Content>
+      <Button>
+        <svg
+          width="20px"
+          height="20px"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="4"
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </Button>
     </CardWrapper>
   );
 };
--- src/styles/PersonCard.js
+++ src/styles/PersonCard.js
@@ -1,8 +1,52 @@
 import styled from 'styled-components';
 
 export const CardWrapper = styled.div`
-  border: 1px solid lightgrey;
-  border-radius: 5px;
-  margin: 2rem;
+  border-radius: 0.5rem;
+  background: #f5f5f5;
+  margin: 1rem;
+  display: flex;
+  justify-content: space-between;
+  justify-items: center;
+`;
+
+export const Content = styled.div`
+  flex: 4;
+  margin: 1rem;
+`;
+
+export const Avatar = styled.img`
+  border: 0.33rem solid #fff;
+  border-radius: 100%;
+  height: 3.5rem;
+  width: 3.5rem;
+  overflow: hidden;
+  margin-right: 1rem;
+  margin: 0.5rem;
+  flex: none;
+`;
+
+export const Name = styled.h4`
+  margin: 0 0 0.5rem;
+  font-weight: bold;
+  color: #666;
+`;
+
+export const Email = styled.div`
+  margin: 0;
+  font-weight: normal;
+  color: #888;
+`;
+
+export const Button = styled.button`
+  margin: 0;
+  background: linear-gradient(
+    to right bottom,
+    rgb(190, 242, 100),
+    rgb(16, 185, 129)
+  );
+  border: 0;
+  color: #fff;
+  overflow: hidden;
+  border-radius: 0 0.5rem 0.5rem 0;
   padding: 1rem;
 `;
