ff04995843d7ec4e14d14531a8fdff00c2a3c78f
matt6gsr <matt6gsr@gmail.com>
2021-02-04 13:59:19 +0000

    added PersonCard

--- src/components/UserCreatedList.jsx
+++ src/components/UserCreatedList.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
 import { ListWrapper } from '../styles/commom';
 import { Header } from './Header';
+import PersonCard from './PersonCard';
 
-const UserCreatedList = () => {
+const UserCreatedList = ({ people, peopleList, userList, setUserList }) => {
   return (
     <ListWrapper>
       <Header text="Please click a person to remove them from your list" list />
+      {userList.length > 0 &&
+        userList.map((user, i) => {
+          console.log('user in map', user);
+          return <PersonCard key={i} person={user} />;
+        })}
     </ListWrapper>
   );
 };
