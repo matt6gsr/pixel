188352bf6a564ff090bdcb8341ea21a3d6a8736c
matt6gsr <matt6gsr@gmail.com>
2021-02-05 12:15:52 +0000

    moved main header back to centre, changed padding via prop

--- src/styles/Header.js
+++ src/styles/Header.js
@@ -6,9 +6,9 @@ export const HeaderText = styled.h3`
 `;
 
 export const HeaderWrapper = styled.div`
-  padding: ${(props) => (props.list ? `2rem 0 0 0` : `2rem`)};
+  padding: ${(props) => (props.list ? `2rem 0 1rem 1rem` : `2rem`)};
   display: flex;
-  justify-content: space-between;
+  justify-content: ${(props) => (props.list ? 'space-between' : 'center')};
   justify-items: center;
   background-color: white;
   border-radius: 5px 5px 0 0;
