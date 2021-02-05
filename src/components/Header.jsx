b631fc44961ecce1208799fe63a79ded197f60af
matt6gsr <matt6gsr@gmail.com>
2021-02-05 12:00:04 +0000

    changed sort function to sort asc and dec, changed button text for svg, edited export syntax

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,52 @@ import React from 'react';
 import { HeaderWrapper, HeaderText } from '../styles/Header';
 import { SortButton } from '../styles/commom';
 
-export const Header = ({ text, list, userList, setUserList }) => {
-  console.log('userList in Header', userList);
-
+const Header = ({
+  text,
+  list,
+  userList,
+  setUserList,
+  setSortOrderAsc,
+  sortOrderAsc,
+}) => {
   return (
     <HeaderWrapper>
       <HeaderText list={list}>{text}</HeaderText>
       {userList && userList.length > 1 && (
         <SortButton
           onClick={() => {
-            console.log('fired');
-            setUserList((userList) =>
-              userList.sort((a, b) => a.name.localeCompare(b.name))
-            );
+            if (sortOrderAsc) {
+              setUserList((userList) =>
+                userList.sort((a, b) => a.name.localeCompare(b.name))
+              );
+            } else {
+              setUserList((userList) =>
+                userList.sort((b, a) => a.name.localeCompare(b.name))
+              );
+            }
+            setSortOrderAsc(!sortOrderAsc);
             console.log('userList in func', userList);
           }}
         >
-          A {`>`} Z
+          <svg
+            width="20px"
+            height="20px"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
+            />
+          </svg>
         </SortButton>
       )}
     </HeaderWrapper>
   );
 };
+
+export default Header;
