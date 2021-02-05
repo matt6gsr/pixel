eb9b3aca3f524b7f9393862a989b836a76aa9b51
matt6gsr <matt6gsr@gmail.com>
2021-02-05 12:53:38 +0000

    changed location of SortButton

--- src/styles/Header.js
+++ src/styles/Header.js
@@ -14,3 +14,26 @@ export const HeaderWrapper = styled.div`
   border-radius: 5px 5px 0 0;
   position: relative;
 `;
+
+export const SortButton = styled.button`
+  border: none;
+  background-image: linear-gradient(
+    to right bottom,
+    rgb(199, 206, 255),
+    rgb(59, 82, 255)
+  );
+  overflow: none;
+  color: #fff;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  position: absolute;
+  top: 1.6rem;
+  right: 1rem;
+
+  :focus {
+    outline: 0;
+  }
+  :hover {
+    cursor: pointer;
+  }
+`;
--- src/styles/commom.js
+++ src/styles/commom.js
@@ -8,26 +8,3 @@ export const ListWrapper = styled.div`
   width: 50%;
   background-color: white;
 `;
-
-export const SortButton = styled.button`
-  border: none;
-  background-image: linear-gradient(
-    to right bottom,
-    rgb(199, 206, 255),
-    rgb(59, 82, 255)
-  );
-  overflow: none;
-  color: #fff;
-  border-radius: 0.5rem;
-  padding: 0.5rem 1rem;
-  position: absolute;
-  top: 1.6rem;
-  right: 1rem;
-
-  :focus {
-    outline: 0;
-  }
-  :hover {
-    cursor: pointer;
-  }
-`;
